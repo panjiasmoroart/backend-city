eb35a9a443d5b1f781a591ad495de46edd8a4b9e
panjiasmoroart <pandjiasmoroart@gmail.com>
2025-11-20 15:24:59 +0700

    RESTful API Photos - Get Data Photos

--- routes/routes.go
+++ routes/routes.go
@@ -77,6 +77,9 @@ func SetupRouter() *gin.Engine {
 		protected.GET("/products/:id", middlewares.Permission("products-show"), adminController.FindProductById)
 		protected.PUT("/products/:id", middlewares.Permission("products-update"), adminController.UpdateProduct)
 		protected.DELETE("/products/:id", middlewares.Permission("products-delete"), adminController.DeleteProduct)
+
+		//Photo routes
+		protected.GET("/photos", middlewares.Permission("photos-index"), adminController.FindPhotos)
 	}
 
 	return router
